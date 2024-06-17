d6b7c413d1f71220f241b5e1e7bb9f4719d450b3
AShakerQA <abdulshaker639@gmail.com>
2024-06-17 19:04:57 +0100

    AS: adding final endpoints

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -33,7 +33,7 @@ export const getRecipeSummary = async (recipeId: string) => {
     throw new Error("API Key not found");
   }
 
-  const url = new URL(
+  const baseUrl = new URL(
     `https://api.spoonacular.com/recipes/${recipeId}/summary`
   );
 
@@ -41,12 +41,36 @@ export const getRecipeSummary = async (recipeId: string) => {
     apiKey: apiKey,
   };
 
-  url.search = new URLSearchParams(queryParams).toString();
+  baseUrl.search = new URLSearchParams(queryParams).toString();
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(baseUrl);
     const resultJson = await response.json();
     return resultJson;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getFavouriteRecipesByIDs = async (ids: string[]) => {
+  if (!apiKey) {
+    throw new Error("API Key not found");
+  }
+
+  const baseUrl = new URL(
+    "https://api.spoonacular.com/recipes/informationBulk"
+  );
+  const queryParams = {
+    apiKey: apiKey,
+    ids: ids.join(","), //structure API endpoint requires
+  };
+
+  baseUrl.search = new URLSearchParams(queryParams).toString();
+
+  try {
+    const response = await fetch(baseUrl);
+    const resultJson = await response.json();
+    return { result: resultJson };
   } catch (error) {
     console.log(error);
   }
