757172332061d5526f12026f49d8c2c29fa5fd41
AShakerQA <abdulshaker639@gmail.com>
2024-06-17 16:21:48 +0100

    AS: adding type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import * as api from "./api";
+import { Recipe } from "./types";
+
 import "./App.css";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-  const handleSearchSubmit = async () => {
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
     try {
       const recipes = await api.searchRecipes(searchTerm, 1);
+
+      setRecipes(recipes.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <div>Hello from recipe App!</div>;
+  return (
+    <div>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <button type="submit">Submit</button>
+      </form>
+
+      {recipes.map((recipe) => (
+        <div>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
