0ad82cd8d1075406b4a5c3e1084af5daed614b3c
AShakerQA <abdulshaker639@gmail.com>
2024-06-17 16:35:09 +0100

    AS: adding recipe cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState, FormEvent } from "react";
 import * as api from "./api";
 import { Recipe } from "./types";
-
+import RecipeCard from "./components/RecipeCard";
 import "./App.css";
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
 
   const handleSearchSubmit = async (event: FormEvent) => {
@@ -21,14 +22,18 @@ const App = () => {
   return (
     <div>
       <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <input
+          type="text"
+          placeholder="Enter a search term..."
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)} //update state with user input
+          required
+        ></input>
         <button type="submit">Submit</button>
       </form>
 
       {recipes.map((recipe) => (
-        <div>
-          recipe image location: {recipe.image}
-          recipe title: {recipe.title}
-        </div>
+        <RecipeCard recipe={recipe} />
       ))}
     </div>
   );
